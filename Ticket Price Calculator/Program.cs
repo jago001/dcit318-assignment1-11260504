3504ba26862175714b1d51c309ac52d81fe4a397
jago001 <jago001@st.ug.edu.gh>
2025-08-05 08:54:26 +0100

    Ticket Price Calculator program

---
+++ Ticket Price Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your age: ");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int age) && age > 0)
+{
+    int price = (age <= 12 || age >= 65) ? 7 : 10;
+    Console.WriteLine($"Your ticket price is GHC{price}.");
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid age.");
+}
